f072a32782c1d59ae931731f160647f80ccb2443
Allison27 <allison_acosta1@usmp.pe>
2018-09-22 18:41:10 -0500

    Creando mi proyecto pokemon

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Alli_bb.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
